390d774757b1cd3246d45a9bdd913973b8f97b66
Prakash Choudhary <prakash@betalectic.com>
2021-02-23 05:47:50 +0530

    Add Heading Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { Heading } from './components/Heading';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Heading = () => {
+  return (
+    <>
+      <h1 className="Heading_title">Critical Illness Insurance Calculator</h1>
+      <p className="Heading_secondary_title">
+        Adjust the sliders to estimate the coverage amount you may need.
+      </p>
+    </>
+  );
+};
