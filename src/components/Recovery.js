83b80b2af49c510a03d267826babe10973e77118
Prakash Choudhary <prakash@betalectic.com>
2021-02-23 09:24:53 +0530

    Add Recovery component

--- src/components/Recovery.js
+++ src/components/Recovery.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 export const Recovery = () => {
   return (
-    <>
-      <p>Recovery</p>
-    </>
+    <div className="Recovery">
+      <p className="Recovery_title">
+        Recovery Chart{' '}
+        <IconContext.Provider
+          className="Recovery_Icon"
+          value={{ color: '#bbb', size: '20px', paddingLeft: '20px' }}
+        >
+          <span>
+            <AiOutlineInfoCircle />
+          </span>
+        </IconContext.Provider>
+      </p>
+      <span className="Recovery_input_container">
+        <input type="number" className="Recovery_input" />
+      </span>
+    </div>
   );
 };
