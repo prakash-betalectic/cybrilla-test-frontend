f3943aac815bb2b1b4972a439f5e993fbe440c3e
Prakash Choudhary <prakash@betalectic.com>
2021-02-23 13:15:25 +0530

    Add state to Recovery component

--- src/components/Recovery.js
+++ src/components/Recovery.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconContext } from 'react-icons';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 export const Recovery = () => {
+  const [recoveryPeriod, setRecoveryPeriod] = useState();
   return (
     <div className="Recovery">
       <p className="Recovery_title">
-        Recovery Chart{' '}
+        Recovery period{' '}
         <IconContext.Provider
           value={{ color: '#bbb', size: '20px', paddingLeft: '20px' }}
         >
@@ -16,7 +17,12 @@ export const Recovery = () => {
         </IconContext.Provider>
       </p>
       <span className="Recovery_input_container">
-        <input type="number" className="Recovery_input" />
+        <input
+          type="number"
+          className="Recovery_input"
+          value={recoveryPeriod}
+          onChange={e => setRecoveryPeriod(e.target.value)}
+        />
       </span>
     </div>
   );
