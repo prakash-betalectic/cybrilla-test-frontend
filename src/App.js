3347c6ee50f83cc588919267b5203562ce51f68c
Prakash Choudhary <prakash@betalectic.com>
2021-02-23 05:59:15 +0530

    Add placeholder Slider, Recovery, Chart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { Heading } from './components/Heading';
+import { Slider } from './components/Slider';
+import { Recovery } from './components/Recovery';
+import { Chart } from './components/Chart';
 
 function App() {
   return (
     <div className="App">
       <Heading />
+      <div className="App_Main_Section">
+        <Slider />
+        <div>
+          <Recovery />
+          <Chart />
+        </div>
+      </div>
     </div>
   );
 }
